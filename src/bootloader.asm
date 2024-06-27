e11f244ec27d23abfe5a44fa36ea625075cb0a9c
parker <129785640+parkerstafford@users.noreply.github.com>
2024-06-26 20:30:26 -0700

    Added protected mode boot

--- src/bootloader.asm
+++ src/bootloader.asm
@@ -2,7 +2,6 @@
 [org 0x7c00]
 
 start:
-    ; Set up segment registers
     cli
     cld
     xor ax, ax
@@ -11,16 +10,59 @@ start:
     mov ss, ax
     mov sp, 0x7c00
 
-    ; Set up the A20 line
+    ; Enable A20 line
     in al, 0x92
     or al, 00000010b
     out 0x92, al
 
-    ; Print start up message to the screen
-    mov si, msg
-    call print_string
+    ; Setup GDT
+    lgdt [gdt_descriptor]
+    ; Enter protected mode
+    mov eax, cr0
+    or eax, 1
+    mov cr0, eax
+    jmp 0x08:protected_mode
 
-    ; Hang
+gdt_start:
+    dw 0x0000
+    dw 0x0000
+    db 0x00
+    db 0x00
+    db 0x00
+    db 0x00
+
+    dw 0xffff
+    dw 0x0000
+    db 0x00
+    db 10011010b
+    db 11001111b
+    db 0x00
+
+    dw 0xffff
+    dw 0x0000
+    db 0x00
+    db 10010010b
+    db 11001111b
+    db 0x00
+
+gdt_descriptor:
+    dw gdt_end - gdt_start - 1
+    dd gdt_start
+gdt_end:
+
+protected_mode:
+    ; Update segment registers
+    mov ax, 0x10
+    mov ds, ax
+    mov es, ax
+    mov fs, ax
+    mov gs, ax
+    mov ss, ax
+    mov esp, 0x9000
+
+    ; Jump to C code
+    call main
+
 hang:
     hlt
     jmp hang
@@ -36,7 +78,7 @@ print_string:
 .done:
     ret
 
-msg db 'TitanBoot has successfully started', 0
+msg db 'Titanboot fully loaded', 0
 
 times 510-($-$$) db 0
 dw 0xaa55
--- src/main.c
+++ src/main.c
@@ -1,6 +1,11 @@
 void main() {
     char *video_memory = (char *)0xb8000;
-    *video_memory = 'X';
-    *(video_memory + 1) = 0x07;
+    const char *msg = "Kernel Loaded!";
+    int i = 0;
+    while (msg[i]) {
+        video_memory[i * 2] = msg[i];
+        video_memory[i * 2 + 1] = 0x07; // White on black
+        i++;
+    }
     while (1) {}
 }
